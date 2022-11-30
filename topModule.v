79669e64037a9d298bdf07752d9fe04928180737
Thiago Batista Araújo <86898996+thibaraujo@users.noreply.github.com>
2022-11-29 21:57:37 -0300

    Add files via upload

--- topModule.v
+++ topModule.v
@@ -1,7 +1,7 @@
 // Aluno: Thiago Batista Araújo
 //Matricula: 2021032968
 
-module topModule (Dados, clk, rst, Disp_RegA, Disp_RegB, Disp_Saida, Instrucao, fim, state, saida_regC);
+module topModule (clk, rst, Dados, Instrucao, Disp_RegA, Disp_RegB, Disp_Saida, state, saida_regC, fim);
 
 input wire [3:0] Dados;
 input clk,rst,Instrucao;
@@ -18,6 +18,9 @@ wire [1:0]Op, Op_Reg;
 wire[4:0] saidaUla;
 
 
+//module mealy (clk,instrucao,fim,enA,enB,op,op_Reg,sel,rst,state);
+mealy Cn (clk,Instrucao,fim,EnA,EnB,Op,Op_Reg,Sel,rst,state);
+
 //module registrador (dados, clk, rst, hab, saida);
 registrador RA(Dados, clk, rst, EnA , saidaA);
 registrador RB(Dados, clk, rst, EnB , saidaB);
@@ -25,19 +28,16 @@ registrador RB(Dados, clk, rst, EnB , saidaB);
 //module mux (in0, in1, sel,saida);
 mux M1({1'b0,saidaB}, saida_regC, Sel,saidaSel);
 
-//module display (Entrada, Hex);
-display D0 ({1'b0,saidaA}, Disp_RegA);
-display D1 ({1'b0,saidaB}, Disp_RegB);
-display D2 (saida_regC, Disp_Saida);
+//module ula (inA, inB, op, saida);
+ula U1({1'b0,saidaA}, saidaSel,Op,saidaUla);
 
 //module regC (in,clk,rst,op_reg,saida);
 regC rc (saidaUla,clk,rst,Op_Reg,saida_regC);
 
-//module ula (inA, inB, op, saida);
-ula U1({1'b0,saidaA}, saidaSel,Op,saidaUla);
-
-//module mealy (clk,instrucao,fim,enA,enB,op,op_Reg,sel,rst,state);
-mealy Cn (clk,Instrucao,fim,EnA,EnB,Op,Op_Reg,Sel,rst,state);
+//module display (Entrada, Hex);
+display D0 ({1'b0,saidaA}, Disp_RegA);
+display D1 ({1'b0,saidaB}, Disp_RegB);
+display D2 (saida_regC, Disp_Saida);
 
 
 endmodule
--- topModule_TB.v
+++ topModule_TB.v
@@ -1,117 +1,88 @@
+// Aluno: Thiago Batista Araújo
+// Matricula: 2021032968
+
 `timescale 1ns/1ps 
+
 module topModule_TB;
 	
-reg [3:0] dadosA=16,dadosB=15,Dados;
-reg Clk,Rst,Ins;
+reg Clk, Rst, Instrucao;	
+reg [3:0] dadosA=16, dadosB=15, Dados;
+
 wire fim; 
-wire [6:0] Hex0,Hex1,Hex2;
+wire [6:0]Hex0, Hex1, Hex2;
 wire [3:0]state;
-wire [4:0]saidaC;
-reg[4:0] Res;
-reg [9:0] Err=0,Acer=0;
+wire [4:0]saida_regC;
+
+
+//module topModule (clk, rst, Dados, Instrucao, Disp_RegA, Disp_RegB, Disp_Saida, state, saida_regC, fim);
+topModule test(Clk, Rst, Dados, Instrucao, Hex0, Hex1, Hex2,  state, saida_regC, fim);
+	
+
+initial begin
+		#39400
+		$finish;
+end
+
+always #7.5 Clk = ~Clk; //inversao de clock a cada 7.5ns
 
 
 
-	
- topModule test(Dados, Clk, Rst,Hex0,Hex1,Hex2,Ins,fim,state,saidaC);
-	
-	initial begin
-		$display("\t\t  Time, Ins, Dado A,  Dado B, Saida,Fim, Res");
-	
+initial begin
 
-		Ins = 1'b0;
-		
-		dadosA=16;
-		dadosB=15;
-		
-		
-		repeat(16) begin
-		
-		dadosA = dadosA-1'b1; 
-		
-		Dados=4'b0000;
-		Clk = 1'b0;
-		Rst = 1'b0;
-		#10 Rst = 1'b1;
-		
-		Dados=dadosA;
-		#10 Dados=dadosB;
-		
-		
-		repeat(16) begin
-		#40 Dados=dadosA;
-		#10 dadosB=dadosB - 1'b1;
-		Dados=dadosB;end
-		
-		end
+	//CICLO INSTRUCAO = 0 => operacao 1
+	Instrucao = 1'b0;
+	//valores iniciais de dados de regA e regB 
+	dadosA=16;
+	dadosB=15;
+	
+	repeat(16) //faz 16 vezes, ou seja, varia de b'1111 a b'0000 
+		begin
+			dadosA = dadosA-1'b1; //variando dados 
+			Dados = 4'b0000; //inicia em 0
+			Clk = 1'b0; 
+			Rst = 1'b0;
+			#15 Rst = 1'b1; //esperar 15ns e rst = 1
 
-
-
-	
-	//##########################################################################################
-	
-	   Ins = 1'b1;
-		
-		dadosA=16;
-		dadosB=15;
-		
-		
-		repeat(16) begin
-		
-		dadosA = dadosA-1'b1; 
-		
-		Dados=4'b0000;
-		Clk = 1'b0;
-		Rst = 1'b0;
-		#10 Rst = 1'b1;
-		
-		Dados=dadosA;
-		#10 Dados=dadosB;
-		
-		
-		repeat(16) begin
-		#40 Dados=dadosA;
-		#10 dadosB=dadosB - 1'b1;
-		Dados=dadosB;end
-		
-		end
-	
-	
-  
+			Dados = dadosA; 
+			#15 Dados = dadosB;
+			
+			repeat(16) 
+				begin
+					#60 Dados=dadosA;
+					#15 dadosB=dadosB - 1'b1;
+					Dados=dadosB;
+				end
 	end
 	
-	always 		#5 	Clk = ~Clk;
+	//a logica eh: toda vez que regB varia de 1111 a 0000, eh decrementado b'1 de regA
+
 	
 	
-	always@(fim)
-	   begin
-	     // O simbolo \t indica uma tabulacao"
-		 
-		  if (Ins)
-			begin 
+	
+	//CICLO INSTRUCAO = 1 => operacao 2 (segue a mesma logica de cima)
+	Instrucao = 1'b1;
+	dadosA=16;
+	dadosB=15;
+	
+	
+	repeat(16) 
+		begin
+			dadosA = dadosA-1'b1; 
+			Dados=4'b0000;
+			Clk = 1'b0;
+			Rst = 1'b0;
+			#15 Rst = 1'b1;
 			
-				Res = ((dadosA + dadosA) - dadosB) ;
-			end 
-			else begin Res = ((dadosA + dadosB)/2) ;end 
-		  
-		  
-		  if (fim==1)
-			begin 
-				if (Res!= saidaC)
-						begin 
-						$display("%d,  %b, \t%d,\t\t\t\t%d ,\t%d , %b, %d ################### Erro, diferente do valor desejado",$time, Ins,dadosA,dadosB,saidaC,fim,Res);
-						Err = Err +1; 
-						end
-				else begin 
-				
-				$display("%d,  %b, \t%d,\t\t\t\t%d ,\t%d , %d, %d",$time, Ins,dadosA,dadosB,saidaC,fim,Res);Acer=Acer+1;end
-			end 
-	 
+			Dados=dadosA;
+			#15 Dados=dadosB;
+			
+			repeat(16) 
+				begin
+					#60 Dados=dadosA;
+					#15 dadosB=dadosB - 1'b1;
+					Dados=dadosB;
+				end
 		end
-	
-	initial begin
-		#26260 $display(" \t\tTotal = %d, Erros = %d , Acertos = %d",(Err+Acer),Err,Acer);
-		$finish;
-		end
+end
 		
-endmodule
+endmodule 
